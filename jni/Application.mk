c5605ed04a1927b862743e3e491b899e76ac0bec
gregko <gregko@hyperionics.com>
2016-08-11 18:32:50 -0400

    With @Voice 9.0.2b

--- jni/Application.mk
+++ jni/Application.mk
@@ -9,7 +9,7 @@ APP_STL := stlport_shared
 ifeq ($(NDK_DEBUG),1)
     APP_OPTIM := debug
 	APP_CFLAGS += -D_DEBUG
-    APP_ABI := x86 armeabi-v7a
+    APP_ABI := x86 mips armeabi armeabi-v7a arm64-v8a
 else 
     APP_OPTIM := release
     APP_ABI := mips armeabi armeabi-v7a arm64-v8a x86
