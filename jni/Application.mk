f45959a93ee40cb90c24d3d2eb8ef0b5f0486887
gregko <gregko@hyperionics.com>
2017-03-28 21:04:05 -0400

    v. 10.10.13b

--- jni/Application.mk
+++ jni/Application.mk
@@ -9,8 +9,8 @@ APP_STL := stlport_shared
 ifeq ($(NDK_DEBUG),1)
     APP_OPTIM := debug
 	APP_CFLAGS += -D_DEBUG
-    APP_ABI := x86 mips armeabi armeabi-v7a arm64-v8a
+    APP_ABI := x86 armeabi-v7a arm64-v8a
 else 
     APP_OPTIM := release
-    APP_ABI := mips armeabi armeabi-v7a arm64-v8a x86
+    APP_ABI := armeabi-v7a arm64-v8a x86
 endif
