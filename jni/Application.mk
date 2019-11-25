8a94dd538c956b776edb0e78d4e85fee3683e476
gregko <gregko@hyperionics.com>
2019-11-25 18:15:17 -0500

    Continue dev.

--- jni/Application.mk
+++ jni/Application.mk
@@ -2,6 +2,7 @@
 NDK_TOOLCHAIN_VERSION := clang
 APP_PLATFORM := android-16
 APP_CFLAGS += -Wno-write-strings -Wno-logical-op-parentheses -Wno-unsequenced -Wno-parentheses -Wno-switch -Wno-#warnings -Wno-invalid-source-encoding
+APP_CFLAGS += -include "C:/android/atVoiceLibs/idiocy_fopen_fd.h"
 APP_CPPFLAGS += -frtti -fexceptions -Wno-logical-op-parentheses -Wno-unsequenced -Wno-parentheses -Wno-switch -Wno-#warnings -Wno-invalid-source-encoding
 APP_ARM_MODE := thumb
 APP_STL := c++_shared
