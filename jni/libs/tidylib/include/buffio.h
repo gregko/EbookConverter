2ebc7f699280332d43cfb975f8d1adb699e31197
gregko <gregko@hyperionics.com>
2020-02-24 08:59:43 -0500

    @Voice 19.3.4

--- jni/libs/tidylib/include/buffio.h
+++
@@ -1,6 +0,0 @@
-#ifdef __GNUC__
-#warning "FIXME: Using compatibility tidy header (buffio.h) that will go away!"
-#endif
-
-#include "tidybuffio.h"
-
--- jni/libs/tidylib/include/platform.h
+++
@@ -1,6 +0,0 @@
-#ifdef __GNUC__
-#warning "FIXME: Using compatibility tidy header (platform.h) that will go away!"
-#endif
-
-#include "tidyplatform.h"
-
